183d16046d96d081739bd635e4e1c4f76ce590fa
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-08-17 16:31:06 +0700

    Add page all-notes, Mobile Header components with his styles and add logo

---
+++ app/all-notes/page.js
@@ -0,0 +1,9 @@
+import MobilePageHeader from "../components/MobilePageHeader";
+
+export default function AllNotes() {
+    return (
+        <div>
+            <MobilePageHeader />
+        </div>
+    )
+}
---
+++ app/components/MobilePageHeader.js
@@ -0,0 +1,17 @@
+import styles from './mobile-page-header.module.css';
+import Image from "next/image";
+
+export default function MobilePageHeader() {
+    return (
+        <header className={`grid-mobile ${styles.mobilePageHeader}`}>
+            <Image 
+                src="/logo.png"
+                alt="Logo de l'application"
+                width={28}
+                height={28}
+                priority
+            />
+            <h1>Notes</h1>
+        </header>
+    )
+}
